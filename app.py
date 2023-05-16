53c542192be4d565aea23707d5169ebc9d935f30
matt638 <131432462+matt638@users.noreply.github.com>
2023-05-16 11:40:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas
 # features list
 features = ['Number_of_vehicles_involved','Number_of_casualties','Hour_of_Day','Type_of_collision','Age_band_of_driver','Sex_of_driver',
     'Educational_level','Service_year_of_vehicle','Day_of_week','Area_accident_occured']
-    # Give a title to web app using html syntax
+# Give a title to web app using html syntax
 st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
 
 # define a main() function to take inputs from user in form based approach
@@ -97,7 +97,7 @@ def main():
        """)
        a,b,c = st.columns([0.2,0.6,0.2])
 with b:
-        st.image("banner-picture.jpeg", use_column_width=True)
+ st.image("banner-picture.jpeg", use_column_width=True)
 
 
 # description about the project and code files       
