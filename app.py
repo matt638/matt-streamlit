7ceb9bfa079b30a636f69ef5a21b6b388cdc21bc
matt638 <131432462+matt638@users.noreply.github.com>
2023-05-16 11:49:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,6 @@ def main():
        [Kaggle](https://www.kaggle.com/avikumart) 
        """)
        a,b,c = st.columns([0.2,0.6,0.2])
-with b:
- st.image("banner-picture.jpeg", use_column_width=True)
 
 
 # description about the project and code files       
